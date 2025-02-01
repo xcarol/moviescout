2f6d0148683c484f5ac6a2de3117018759558d5f
xcarol <xcarolr@gmail.com>
2025-02-01 18:57:53 +0100

    Refactor TmdbService to support multi-search and extract titles from response

--- lib/services/tmdb.dart
+++ lib/services/tmdb.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 const String _tmdbSearch =
-    'https://api.themoviedb.org/3/search/movie?query={SEARCH}&page=1&language={LOCALE}';
+    'https://api.themoviedb.org/3/search/multi?query={SEARCH}&page=1&language={LOCALE}';
 
 class TmdbService {
-  Future<dynamic> searchMovie(
+  Future<dynamic> searchTitle(
     String search,
     Locale locale,
   ) async {
@@ -30,7 +30,7 @@ class TmdbService {
     });
 
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+      return getTitles(json.decode(response.body));
     } else {
       Exception exception = Exception(
         [
@@ -42,5 +42,22 @@ class TmdbService {
           .recordFlutterError(FlutterErrorDetails(exception: exception));
       throw exception;
     }
+  }
+
+  List getTitles(Map response) {
+    List titles = [];
+    for (int count = 0;
+        count < response['results'].length && count < 10;
+        count += 1) {
+      final title = response['results'][count];
+      titles.add({
+        'id': title['id'],
+        'title': title['title'] ?? title['name'],
+        'poster_path': title['poster_path'],
+        'overview': title['overview'], 
+        'release_date': title['release_date'],
+      });
+    }
+    return titles;
   }
 }
