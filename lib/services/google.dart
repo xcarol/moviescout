aad5ed66c2262dbd4cc7f5e4afe502e69029d1cc
xcarol <xcarolr@gmail.com>
2025-02-14 18:35:10 +0100

    Implement Google Sign-In service and update app drawer for user authentication

--- lib/services/google.dart
+++ lib/services/google.dart
@@ -1,34 +1,56 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<User?> signInWithGoogle() async {
-  try {
-    final GoogleSignInAccount? googleUser =
-        await GoogleSignIn(clientId: dotenv.env['OAUTH_CLIENT_ID']).signIn();
-    if (googleUser == null) return null; // Usuari cancel·la el login
+const List<String> scopes = <String>['email'];
+late GoogleSignIn _googleSignIn;
 
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    final OAuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
+GoogleSignInService defaultAppInstance = GoogleSignInService();
 
-    final UserCredential userCredential =
-        await FirebaseAuth.instance.signInWithCredential(credential);
-    return userCredential.user;
-  } catch (e) {
-    print("Error en Google Sign-In: $e");
-    return null;
+class GoogleSignInService {
+  GoogleSignInAccount? get currentUser => _googleSignIn.currentUser;
+
+  static GoogleSignInService get instance {
+    return defaultAppInstance;
   }
-}
 
-Future<void> signOutGoogle() async {
-  try {
-    await GoogleSignIn(clientId: dotenv.env['OAUTH_CLIENT_ID']).signOut();
-    await FirebaseAuth.instance.signOut();
-  } catch (e) {
-    print("Error in signOutGoogle: $e");
+  initService() async {
+    if (kIsWeb) {
+      _googleSignIn = GoogleSignIn(
+        clientId: dotenv.env['OAUTH_CLIENT_ID'],
+        scopes: scopes,
+      );
+    } else {
+      _googleSignIn = GoogleSignIn(
+        serverClientId: dotenv.env['OAUTH_CLIENT_ID'],
+        scopes: scopes,
+      );
+    }
+
+    _googleSignIn.onCurrentUserChanged
+        .listen((GoogleSignInAccount? account) async {
+      // In mobile, being authenticated means being authorized...
+      // But in web, if more persmissions are required at some time, 
+      // this is the place to check for them using canAccessScopes method.
+      // Check it out at https://pub.dev/packages/google_sign_in_web
+    });
+
+    _googleSignIn.signInSilently();
+  }
+
+  Future<void> signIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _googleSignIn.disconnect();
+    } catch (e) {
+      print("Error in signOutGoogle: $e");
+    }
   }
 }
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,54 +1,85 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:google_sign_in_web/web_only.dart' as web;
 import 'package:moviescout/services/google.dart';
+
+typedef HandleSignInFn = Future<void> Function();
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    User? user = FirebaseAuth.instance.currentUser;
+    GoogleSignInAccount? user = GoogleSignInService.instance.currentUser;
+    NetworkImage? userImage = NetworkImage(user?.photoUrl ?? '');
 
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
           UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: userImage,
+            ),
             decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.inversePrimary,
             ),
             accountName: Text(
               user?.displayName ?? AppLocalizations.of(context)!.anonymousUser,
             ),
-            accountEmail: Text(user?.emailVerified == true ? user!.email! : ''),
+            accountEmail: Text(user?.email ?? ''),
           ),
-          ListTile(
-            leading: Icon(user != null ? Icons.logout : Icons.login),
-            title: Text(user != null
-                ? AppLocalizations.of(context)!.logout
-                : AppLocalizations.of(context)!.login),
-            onTap: () => {
-              Navigator.of(context).pop(),
-              if (user != null) {logout()} else {login()}
-            },
-          ),
+          if (!kIsWeb)
+            ListTile(
+              leading: Icon(user != null ? Icons.logout : Icons.login),
+              title: Text(user != null
+                  ? AppLocalizations.of(context)!.logout
+                  : AppLocalizations.of(context)!.login),
+              onTap: () => {
+                Navigator.of(context).pop(),
+                if (user != null) {logout()} else {login()}
+              },
+            ),
+          if (kIsWeb && user == null)
+            ListTile(
+              leading: buildSignInButton(),
+              title: Text(AppLocalizations.of(context)!.login),
+            ),
+          if (kIsWeb && user != null)
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text(AppLocalizations.of(context)!.logout),
+              onTap: () => {
+                Navigator.of(context).pop(),
+                logout(),
+              },
+            ),
         ],
       ),
     );
   }
 
-  login() async {
-    User? user = await signInWithGoogle();
-    if (user != null) {
-      print("Sessió iniciada amb: ${user.displayName}");
-    } else {
-      print("Error en iniciar sessió.");
+  Widget buildSignInButton({HandleSignInFn? onPressed}) {
+    return web.renderButton(
+      configuration: GSIButtonConfiguration(
+        type: GSIButtonType.icon,
+      ),
+    );
+  }
+
+  Future<void> login() async {
+    try {
+      await GoogleSignInService.instance.signIn();
+    } catch (error) {
+      print(error);
     }
   }
 
   logout() async {
-    await signOutGoogle();
+    await GoogleSignInService.instance.signOut();
     print("Sessió finalitzada.");
   }
 }
