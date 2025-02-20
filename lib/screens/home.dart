65b0f5695547796680807bfb3aba4a186128f2b8
xcarol <xcarolr@gmail.com>
2025-02-20 18:55:14 +0100

    Load watchlist titles and display them in the home screen; refactor body rendering logic

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:moviescout/services/google.dart';
 import 'package:moviescout/screens/search.dart';
 import 'package:moviescout/widgets/app_bar.dart';
 import 'package:moviescout/widgets/app_drawer.dart';
+import 'package:moviescout/widgets/title_list.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,6 +14,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late List<int> watchlistTitles = List.empty();
+
+  @override
+  void initState() {
+    super.initState();
+    loadWatchlistTitles();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,30 +42,58 @@ class _HomeState extends State<Home> {
         ],
       ),
       drawer: AppDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.messageEmptyList,
-                ),
-                Text(
-                  AppLocalizations.of(context)!.messageEmptyList2,
-                ),
-              ],
+      body: Center(child: homeBody()),
+    );
+  }
+
+  Widget homeBody() {
+    if (watchlistTitles.isEmpty) {
+      return emptyBody();
+    } else {
+      return watchlistBody();
+    }
+  }
+
+  Widget emptyBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          children: [
+            Text(
+              AppLocalizations.of(context)!.messageEmptyList,
+            ),
+            Text(
+              AppLocalizations.of(context)!.messageEmptyList2,
             ),
           ],
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget watchlistBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TitleList(titles: watchlistTitles),
+      ],
     );
   }
 
   searchTitle() async {
+    loadWatchlistTitles();
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => Search()),
     );
+  }
+
+  void loadWatchlistTitles() async {
+    List<int> titles =
+        await GoogleService.instance.readWatchlistTitles(context);
+    setState(() {
+      watchlistTitles = titles;
+    });
   }
 }
