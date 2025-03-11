b4a47b15d6285d88ab27d8a661e25831a8a44b1c
xcarol <xcarolr@gmail.com>
2025-03-11 17:15:17 +0100

    Implement updateWatchlistTitle  in TmdbWatchlistService

--- lib/services/tmdb_watchlist_service.dart
+++ lib/services/tmdb_watchlist_service.dart
@@ -2,9 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:moviescout/services/tmdb_base_service.dart';
 
 class TmdbWatchlistService extends TmdbBaseService with ChangeNotifier {
-  List userWatchlist = List.empty();
+  List userWatchlist = List.empty(growable: true);
 
-  void updateWatchlist(Map title, bool add) {
+  Future<dynamic> _updateTitleInWatchlistToTmdb(
+      int accountId, int id, bool add) async {
+    return post('account/$accountId/watchlist',
+        {'media_type': 'movie', 'media_id': id, 'watchlist': add});
+  }
+
+  Future<void> updateWatchlistTitle(int accountId, Map title, bool add) async {
+    if (add) {
+      final result =
+          await _updateTitleInWatchlistToTmdb(accountId, title['id'], true);
+      if (result['success'] == true) {
+        userWatchlist.add(title);
+      } else {
+        throw Exception('Failed to add title to watchlist. Response: $result');
+      }
+    } else {
+      final result =
+          await _updateTitleInWatchlistToTmdb(accountId, title['id'], false);
+      if (result['success'] == true) {
+        userWatchlist.removeWhere((element) => element['id'] == title['id']);
+      } else {
+        throw Exception('Failed to remove title to watchlist. Response: $result');
+      }
+    }
     notifyListeners();
   }
 }
