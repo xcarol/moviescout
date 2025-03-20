34f8c2d91ac97284987dc60b9d6d913d62d33e7e
xcarol <xcarolr@gmail.com>
2025-03-20 17:11:22 +0100

    refactor: update TmdbTitleService to use TmdbTitle model for improved type safety and clarity

--- lib/services/tmdb_title_service.dart
+++ lib/services/tmdb_title_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:moviescout/models/tmdb_title.dart';
 import 'package:moviescout/services/tmdb_base_service.dart';
 
 const String _tmdbDetails = '/{MEDIA_TYPE}/{ID}?language={LOCALE}';
@@ -27,47 +28,50 @@ class TmdbTitleService extends TmdbBaseService {
     Locale locale,
   ) async {
     for (int count = 0; count < titles.length; count += 1) {
-      final title = titles[count];
-      final details = await getTitleDetails(title, locale);
+      final TmdbTitle title = titles[count];
+      final TmdbTitle details = await getTitleDetails(title, locale);
       titles[count] = details;
     }
     return titles;
   }
 
-  Future<dynamic> getTitleDetails(
-    Map title,
+  Future<TmdbTitle> getTitleDetails(
+    TmdbTitle title,
     Locale locale,
   ) async {
-    if (title['last_updated'] != null &&
-        DateTime.now()
-                .difference(DateTime.parse(title['last_updated']))
-                .inDays <
-            DateTime.daysPerWeek) {
+    if (DateTime.now()
+            .difference(
+              DateTime.parse(title.lastUpdated),
+            )
+            .inDays <
+        DateTime.daysPerWeek) {
       return title;
     }
 
-    String mediaType = title['media_type'] ?? '';
+    String mediaType = title.mediaType;
     if (mediaType == '') {
-      mediaType = title['title'] != null ? 'movie' : 'tv';
+      mediaType = title.isMovie ? 'movie' : 'tv';
     }
 
     final result = await get(
       _tmdbDetails
           .replaceFirst('{MEDIA_TYPE}', mediaType)
-          .replaceFirst('{ID}', title['id'].toString())
+          .replaceFirst('{ID}', title.id.toString())
           .replaceFirst(
               '{LOCALE}', '${locale.languageCode}-${locale.countryCode}'),
     );
+
     if (result.statusCode == 200) {
       final titleDetails = body(result);
 
       titleDetails['media_type'] = mediaType;
       titleDetails['providers'] =
-          await getTitleProviders(title['id'], mediaType);
+          await getTitleProviders(title.id, mediaType);
       titleDetails['last_updated'] = DateTime.now().toIso8601String();
 
-      return titleDetails;
+      return TmdbTitle(title: titleDetails);
     }
-    return {};
+    
+    return TmdbTitle(title: {});
   }
 }
