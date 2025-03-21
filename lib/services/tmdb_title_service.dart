cb341a106609a53c63631544892c7095f478a8b0
xcarol <xcarolr@gmail.com>
2025-03-21 18:53:44 +0100

    refactor: encapsulate country and language code retrieval, and simplify provider fetching logic in TmdbTitleService

--- lib/services/tmdb_title_service.dart
+++ lib/services/tmdb_title_service.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:moviescout/models/tmdb_title.dart';
 import 'package:moviescout/services/tmdb_base_service.dart';
 
@@ -7,44 +6,48 @@ const String _tmdbDetails = '/{MEDIA_TYPE}/{ID}?language={LOCALE}';
 const String _tmdbProviders = '/{MEDIA_TYPE}/{ID}/watch/providers';
 
 class TmdbTitleService extends TmdbBaseService {
-  String getCountryCode() {
+  String _getCountryCode() {
     return PlatformDispatcher.instance.locale.countryCode ?? "US";
   }
 
-  getTitleProviders(int titleId, String mediaType) async {
+  String _getLanguageCode() {
+    return PlatformDispatcher.instance.locale.languageCode;
+  }
+
+  bool _isUpToDate(TmdbTitle title) {
+    return DateTime.now()
+            .difference(
+              DateTime.parse(title.lastUpdated),
+            )
+            .inDays <
+        DateTime.daysPerWeek;
+  }
+
+  _getProviders(int titleId, String mediaType) async {
     final result = await get(
       _tmdbProviders
           .replaceFirst('{MEDIA_TYPE}', mediaType)
           .replaceFirst('{ID}', titleId.toString()),
     );
     if (result.statusCode == 200) {
-      return body(result)['results'][getCountryCode()];
+      return body(result)['results'][_getCountryCode()];
     }
     return {};
   }
 
   Future<dynamic> getTitlesDetails(
     List titles,
-    Locale locale,
   ) async {
     for (int count = 0; count < titles.length; count += 1) {
       final TmdbTitle title = titles[count];
-      final TmdbTitle details = await getTitleDetails(title, locale);
+      final TmdbTitle details = await getTitleDetails(title);
       titles[count] = details;
     }
     return titles;
   }
 
-  Future<TmdbTitle> getTitleDetails(
-    TmdbTitle title,
-    Locale locale,
-  ) async {
-    if (DateTime.now()
-            .difference(
-              DateTime.parse(title.lastUpdated),
-            )
-            .inDays <
-        DateTime.daysPerWeek) {
+  Future<TmdbTitle> getTitleDetails(TmdbTitle title) async {
+    if (_isUpToDate(title)) {
       return title;
     }
 
@@ -58,20 +61,19 @@ class TmdbTitleService extends TmdbBaseService {
           .replaceFirst('{MEDIA_TYPE}', mediaType)
           .replaceFirst('{ID}', title.id.toString())
           .replaceFirst(
-              '{LOCALE}', '${locale.languageCode}-${locale.countryCode}'),
+              '{LOCALE}', '${_getLanguageCode()}-${_getCountryCode()}'),
     );
 
     if (result.statusCode == 200) {
       final titleDetails = body(result);
 
       titleDetails['media_type'] = mediaType;
-      titleDetails['providers'] =
-          await getTitleProviders(title.id, mediaType);
+      titleDetails['providers'] = await _getProviders(title.id, mediaType);
       titleDetails['last_updated'] = DateTime.now().toIso8601String();
 
       return TmdbTitle(title: titleDetails);
     }
-    
+
     return TmdbTitle(title: {});
   }
 }
