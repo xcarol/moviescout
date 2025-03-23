a6b71a875614109d35a897956b415af43706408c
xcarol <xcarolr@gmail.com>
2025-03-23 18:04:01 +0100

    feat: add title type selector for all types, movies, and tv shows

--- lib/widgets/title_list.dart
+++ lib/widgets/title_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:moviescout/models/tmdb_title.dart';
 import 'package:moviescout/services/snack_bar.dart';
 import 'package:moviescout/services/tmdb_user_service.dart';
@@ -20,9 +21,50 @@ class TitleList extends StatefulWidget {
 
 class _TitleListState extends State<TitleList> {
   int updatingTitleId = 0;
+  late String selectedType;
+  late List<String> titleTypes;
 
   @override
-  Widget build(BuildContext context) {
+  didChangeDependencies() {
+    super.didChangeDependencies();
+    selectedType = AppLocalizations.of(context)!.allTypes;
+    titleTypes = [
+      AppLocalizations.of(context)!.allTypes,
+      AppLocalizations.of(context)!.movies,
+      AppLocalizations.of(context)!.tvshows,
+    ];
+  }
+
+  Widget typeSelector() {
+    return DropdownButton<String>(
+      value: selectedType,
+      items: titleTypes.map((title) {
+        return DropdownMenuItem(
+          value: title,
+          child: Text(title),
+        );
+      }).toList(),
+      onChanged: (newValue) {
+        setState(() {
+          selectedType = newValue!;
+        });
+      },
+    );
+  }
+
+  Widget listControls() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          typeSelector(),
+        ],
+      ),
+    );
+  }
+
+  Widget titleList() {
     return Expanded(
       child: ListView.builder(
         key: PageStorageKey('TitleListView'),
@@ -34,32 +76,46 @@ class _TitleListState extends State<TitleList> {
                   .userWatchlist
                   .any((t) => t.id == title.id);
           return TitleCard(
-              context: context,
-              title: title,
-              isUpdating: updatingTitleId == title.id,
-              isInWatchlist: isInWatchlist,
-              onPressed: () {
-                setState(() {
-                  updatingTitleId = title.id;
-                });
-                Provider.of<TmdbWatchlistService>(context, listen: false)
-                    .updateWatchlistTitle(
-                        Provider.of<TmdbUserService>(context, listen: false)
-                            .accountId,
-                        title,
-                        !isInWatchlist)
-                    .then((value) async {
-                  setState(() {
-                    updatingTitleId = 0;
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    updatingTitleId = 0;
-                  });
-                  SnackMessage.showSnackBar(error.toString());
-                });
+            context: context,
+            title: title,
+            isUpdating: updatingTitleId == title.id,
+            isInWatchlist: isInWatchlist,
+            onPressed: () {
+              setState(() {
+                updatingTitleId = title.id;
               });
+              Provider.of<TmdbWatchlistService>(context, listen: false)
+                  .updateWatchlistTitle(
+                      Provider.of<TmdbUserService>(context, listen: false)
+                          .accountId,
+                      title,
+                      !isInWatchlist)
+                  .then((value) async {
+                setState(() {
+                  updatingTitleId = 0;
+                });
+              }).catchError((error) {
+                setState(() {
+                  updatingTitleId = 0;
+                });
+                SnackMessage.showSnackBar(error.toString());
+              });
+            },
+          );
         },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          listControls(),
+          titleList(),
+        ],
       ),
     );
   }
