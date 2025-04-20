05fee45b4454bff21330fdc8354f045cfbba6483
xcarol <xcarolr@gmail.com>
2025-04-20 19:27:51 +0200

    refactor: simplify data loading in RatesList by using Future in initState. Refactor to listen and consume data from TmdbRateslistService

--- lib/screens/rates_list.dart
+++ lib/screens/rates_list.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:moviescout/screens/search.dart';
 import 'package:moviescout/services/tmdb_rateslist_service.dart';
-import 'package:moviescout/services/tmdb_title_service.dart';
 import 'package:moviescout/services/tmdb_user_service.dart';
-import 'package:moviescout/services/tmdb_watchlist_service.dart';
 import 'package:moviescout/widgets/app_bar.dart';
 import 'package:moviescout/widgets/app_drawer.dart';
 import 'package:moviescout/widgets/bottom_bar.dart';
@@ -19,47 +17,55 @@ class RatesList extends StatefulWidget {
 }
 
 class _RatesListState extends State<RatesList> {
+  late Future<void> _init;
+
+  @override
+  void initState() {
+    super.initState();
+    _init = _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final userService = Provider.of<TmdbUserService>(context, listen: false);
+    final rateslistService =
+        Provider.of<TmdbRateslistService>(context, listen: false);
+
+    await userService.setup();
+    await rateslistService.retrieveRates(userService.accountId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Future.wait([
-          Provider.of<TmdbUserService>(context, listen: false)
-              .setup()
-              .then((_) {
-            if (context.mounted) {
-              return Provider.of<TmdbRateslistService>(context, listen: false)
-                  .retrieveRateslist(
-                      Provider.of<TmdbUserService>(context, listen: false)
-                          .accountId);
-            } else {
-              return null;
-            }
-          }),
-        ]),
-        builder: (context, snapshot) {
-          if (snapshot.data == null) {
-            return const Scaffold();
-          }
-          return Scaffold(
-            appBar: MainAppBar(
-              context: context,
-              title: AppLocalizations.of(context)!.rateslistTitle,
-            ),
-            drawer: AppDrawer(),
-            body: Center(child: body()),
-            bottomNavigationBar:
-                BottomBar(currentIndex: BottomBarIndex.indexRateslist),
-          );
-        });
+      future: _init,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Scaffold(
+              body: Center(child: CircularProgressIndicator()));
+        }
+
+        return Consumer<TmdbRateslistService>(
+          builder: (context, rateslistService, _) {
+            return Scaffold(
+              appBar: MainAppBar(
+                context: context,
+                title: AppLocalizations.of(context)!.rateslistTitle,
+              ),
+              drawer: AppDrawer(),
+              body: Center(child: body()),
+              bottomNavigationBar:
+                  BottomBar(currentIndex: BottomBarIndex.indexRateslist),
+            );
+          },
+        );
+      },
+    );
   }
 
   Widget body() {
-    return ListenableBuilder(
-      listenable: Provider.of<TmdbWatchlistService>(context, listen: false),
-      builder: (BuildContext context, Widget? child) {
-        if (Provider.of<TmdbWatchlistService>(context, listen: false)
-            .watchlist
-            .isEmpty) {
+    return Consumer<TmdbRateslistService>(
+      builder: (context, rateslistService, child) {
+        if (rateslistService.titles.isEmpty) {
           return emptyBody();
         } else {
           return rateslistBody();
@@ -84,23 +90,13 @@ class _RatesListState extends State<RatesList> {
   }
 
   Widget rateslistBody() {
-    return FutureBuilder(
-      future: TmdbTitleService().getTitlesDetails(
-          Provider.of<TmdbRateslistService>(context, listen: false).rateslist),
-      builder: (context, AsyncSnapshot<dynamic> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text("Error: ${snapshot.error}"));
-        } else {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TitleList(titles: snapshot.data!),
-            ],
-          );
-        }
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TitleList(
+            titles: Provider.of<TmdbRateslistService>(context, listen: false)
+                .titles),
+      ],
     );
   }
 
