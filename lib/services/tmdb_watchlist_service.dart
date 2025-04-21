c47346b6944ec80b1480c188ba0639047a5947e7
xcarol <xcarolr@gmail.com>
2025-04-21 16:23:03 +0200

    refactor: streamline watchlist retrieval and update methods in TmdbWatchlistService to extend from TmdbListService

--- lib/services/tmdb_watchlist_service.dart
+++ lib/services/tmdb_watchlist_service.dart
@@ -1,111 +1,26 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
 import 'package:moviescout/models/tmdb_title.dart';
-import 'package:moviescout/services/preferences_service.dart';
-import 'package:moviescout/services/tmdb_base_service.dart';
+import 'package:moviescout/services/snack_bar.dart';
+import 'package:moviescout/services/tmdb_list_service.dart';
 
-String _prefsWatchlistName = 'watchlist';
-
-class TmdbWatchlistService extends TmdbBaseService with ChangeNotifier {
-  List<TmdbTitle> watchlist = List.empty(growable: true);
-
-  void clearWatchList() {
-    watchlist = List.empty(growable: true);
-    _updateWatchlistInCache();
-    notifyListeners();
-  }
+class TmdbWatchlistService extends TmdbListService {
+  TmdbWatchlistService(super.listName);
 
   Future<void> retrieveWatchlist(String accountId) async {
-    if (accountId.isEmpty) {
-      return;
-    }
-
-    watchlist = _retrieveWatchlistFromCache();
-    if (watchlist.isNotEmpty) {
-      await _syncCacheWatchListWithServer(accountId);
-    } else {
-      watchlist = await _retrieveWatchlistFromServer(accountId);
-      _updateWatchlistInCache();
-    }
-
-    notifyListeners();
-  }
-
-  Future<dynamic> _retrieveWatchlistFromServer(String accountId) async {
-    Map<String, dynamic> movies = {};
-    dynamic response = await get('account/$accountId/watchlist/movies');
-    if (response.statusCode == 200) {
-      movies = body(response);
-    }
-
-    Map<String, dynamic> tv = {};
-    response = await get('account/$accountId/watchlist/tv');
-    if (response.statusCode == 200) {
-      tv = body(response);
-    }
-
-    List<TmdbTitle> watchlist = List.empty(growable: true);
-
-    if (movies['results'] != null) {
-      movies['results'].forEach((element) {
-        element['media_type'] = 'movie';
-        watchlist.add(TmdbTitle(title: element));
-      });
-    }
-
-    if (tv['results'] != null) {
-      tv['results'].forEach((element) {
-        element['media_type'] = 'tv';
-        watchlist.add(TmdbTitle(title: element));
-      });
-    }
-
-    return watchlist;
-  }
-
-  List<TmdbTitle> _retrieveWatchlistFromCache() {
-    final List<String>? watchlistJson =
-        PreferencesService().prefs.getStringList(_prefsWatchlistName);
-
-    if (watchlistJson == null || watchlistJson.isEmpty) {
-      return List.empty(growable: true);
-    }
-
-    return watchlistJson
-        .map((json) => TmdbTitle(title: jsonDecode(json)))
-        .toList();
-  }
-
-  Future<void> _syncCacheWatchListWithServer(String accountId) async {
-    List<TmdbTitle> cacheWatchlist = _retrieveWatchlistFromCache();
-    List<TmdbTitle> serverWatchlist =
-        await _retrieveWatchlistFromServer(accountId);
-
-    List<TmdbTitle> titlesToAdd = serverWatchlist
-        .where((title) => !cacheWatchlist.contains(title))
-        .toList();
-
-    List<TmdbTitle> titlesToRemove = cacheWatchlist
-        .where((title) => !serverWatchlist.contains(title))
-        .toList();
-
-    for (TmdbTitle title in titlesToAdd) {
-      watchlist.add(title);
-    }
-    for (TmdbTitle title in titlesToRemove) {
-      watchlist.removeWhere((element) => element.id == title.id);
-    }
-    _updateWatchlistInCache();
-  }
-
-  Future<void> _updateWatchlistInCache() async {
-    List<String> watchlistJson =
-        watchlist.map((title) => jsonEncode(title.map)).toList();
-
-    PreferencesService()
-        .prefs
-        .setStringList(_prefsWatchlistName, watchlistJson);
+    retrieveList(accountId, retrieveMovies: () async {
+      late Map<String, dynamic> movies = {};
+      dynamic response = await get('account/$accountId/watchlist/movies');
+      if (response.statusCode == 200) {
+        movies = body(response);
+      }
+      return movies;
+    }, retrieveTvshows: () async {
+      Map<String, dynamic> tv = {};
+      dynamic response = await get('account/$accountId/watchlist/tv');
+      if (response.statusCode == 200) {
+        tv = body(response);
+      }
+      return tv;
+    });
   }
 
   Future<dynamic> _updateTitleInWatchlistToTmdb(
@@ -116,26 +31,15 @@ class TmdbWatchlistService extends TmdbBaseService with ChangeNotifier {
 
   Future<void> updateWatchlistTitle(
       String accountId, TmdbTitle title, bool add) async {
-    if (add) {
-      final result = await _updateTitleInWatchlistToTmdb(
-          accountId, title.id, title.mediaType, true);
-      if (result.statusCode == 201) {
-        watchlist.add(title);
-      } else {
-        throw Exception(
-            'Failed to add title to watchlist. Response code: ${result.statusCode}');
-      }
-    } else {
-      final result = await _updateTitleInWatchlistToTmdb(
-          accountId, title.id, title.mediaType, false);
-      if (result.statusCode == 200) {
-        watchlist.removeWhere((element) => element.id == title.id);
-      } else {
-        throw Exception(
-            'Failed to remove title to watchlist. Response code: ${result.statusCode}');
-      }
+    try {
+      await updateTitle(accountId, title, add, (String accountId) async {
+        return _updateTitleInWatchlistToTmdb(
+            accountId, title.id, title.mediaType, add);
+      });
+    } catch (error) {
+      SnackMessage.showSnackBar(
+        'Error updating watchlist for ${title.name}: $error',
+      );
     }
-    _updateWatchlistInCache();
-    notifyListeners();
   }
 }
