19c4e1f95f715a98df880ffffe8f76f9693e6495
xcarol <xcarolr@gmail.com>
2025-05-21 16:09:34 +0200

    feat: add selected and notSelected colors to CustomColors theme extension

--- lib/models/custom_colors.dart
+++ lib/models/custom_colors.dart
@@ -5,11 +5,15 @@ class CustomColors extends ThemeExtension<CustomColors> {
   final Color inWatchlist;
   final Color notInWatchlist;
   final Color ratedTitle;
+  final Color selected;
+  final Color notSelected;
 
   const CustomColors({
     required this.inWatchlist,
     required this.notInWatchlist,
     required this.ratedTitle,
+    required this.selected,
+    required this.notSelected,
   });
 
   @override
@@ -17,11 +21,15 @@ class CustomColors extends ThemeExtension<CustomColors> {
     Color? inWatchlist,
     Color? notInWatchlist,
     Color? ratedTitle,
+    Color? selected,
+    Color? notSelected,
   }) {
     return CustomColors(
       inWatchlist: inWatchlist ?? this.inWatchlist,
       notInWatchlist: notInWatchlist ?? this.notInWatchlist,
       ratedTitle: ratedTitle ?? this.ratedTitle,
+      selected: selected ?? this.selected,
+      notSelected: notSelected ?? this.notSelected,
     );
   }
 
@@ -32,6 +40,8 @@ class CustomColors extends ThemeExtension<CustomColors> {
       inWatchlist: Color.lerp(inWatchlist, other.inWatchlist, t)!,
       notInWatchlist: Color.lerp(notInWatchlist, other.notInWatchlist, t)!,
       ratedTitle: Color.lerp(ratedTitle, other.ratedTitle, t)!,
+      selected: Color.lerp(selected, other.selected, t)!,
+      notSelected: Color.lerp(notSelected, other.notSelected, t)!,
     );
   }
 }
