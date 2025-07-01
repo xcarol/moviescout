9a32f7aaaaf7a7cf07febb0c957566f0e1b471e1
xcarol <xcarolr@gmail.com>
2025-07-01 15:32:25 +0200

    Add localization support for multiple languages
    - Created localization files for Catalan, English, and Spanish.
    - Implemented the AppLocalizations class to manage localized strings.
    - Updated main.dart and various screen files to use the new localization structure.
    - Replaced flutter_gen imports with local imports for app_localizations.dart.

---
+++ lib/l10n/app_localizations_ca.dart
@@ -0,0 +1,214 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Catalan Valencian (`ca`).
+class AppLocalizationsCa extends AppLocalizations {
+  AppLocalizationsCa([String locale = 'ca']) : super(locale);
+
+  @override
+  String get appTitle => 'Movie Scout';
+
+  @override
+  String get messageEmptyList => 'Encara no s\'ha escollit cap pel·lícula.';
+
+  @override
+  String get messageEmptyList2 => 'Fes una cerca utilitzant la lupa de la barra inferior.';
+
+  @override
+  String get messageEmptyList3 => 'Inicia sessió o registra\'t a TMDB des del menú lateral.';
+
+  @override
+  String get search => 'Cerca un títol';
+
+  @override
+  String get searchTitle => 'Cerca';
+
+  @override
+  String get back => 'Enrere';
+
+  @override
+  String get username => 'Usuari';
+
+  @override
+  String get password => 'Contrasenya';
+
+  @override
+  String get anonymousUser => 'Sense usuari';
+
+  @override
+  String get loginTitle => 'Inici de sessió';
+
+  @override
+  String get loginDescription => 'Inicia sessió al teu compte de TMDB';
+
+  @override
+  String get login => 'Inicia sessió';
+
+  @override
+  String get logout => 'Tanca sessió';
+
+  @override
+  String get loginSuccess => 'Sessió iniciada correctament.';
+
+  @override
+  String get loginFailed => 'Usuari o contrasenya incorrectes.';
+
+  @override
+  String get logoutSuccess => 'Sessió tancada correctament.';
+
+  @override
+  String get loginToTmdb => 'Inicia sessió amb TMDb';
+
+  @override
+  String get completeLoginToTmdb => 'Completa l\'inici de sessió';
+
+  @override
+  String get signupToTmdb => 'Registra\'t a TMDb';
+
+  @override
+  String get signInToWatchlist => 'Cal iniciar sessió per afegir títols.';
+
+  @override
+  String get tvShow => 'Sèrie';
+
+  @override
+  String get movie => 'Pel·lícula';
+
+  @override
+  String get select => 'Sel·lecciona';
+
+  @override
+  String get imdbImport => 'Importar de IMDB';
+
+  @override
+  String get imdbImportHint => 'Sel·lecciona un arxiu CSV exportat de IMDB';
+
+  @override
+  String get imdbImportWatchlist => 'Importar Per Veure';
+
+  @override
+  String get imdbImportRateslist => 'Importar Valoracions';
+
+  @override
+  String get imdbImportCount => 'Títols importats correctament';
+
+  @override
+  String get imdbResetWatchlist => 'ELIMINAR PER VEURE';
+
+  @override
+  String get imdbResetRateslist => 'ELIMINAR VALORACIONS';
+
+  @override
+  String get imdbConfirmationTitle => 'ATENCIÓ';
+
+  @override
+  String get imdbResetWatchlistConfirmation => 'Segur que vols eliminar els títols Per Veure?';
+
+  @override
+  String get imdbResetRateslistConfirmation => 'Segur que vols eliminar les Valoracions?';
+
+  @override
+  String get resetWatchlistCount => 'Títols Per Veure: ';
+
+  @override
+  String get resetRateslistCount => 'Títols Valorats: ';
+
+  @override
+  String get missingDescription => 'Falta la descripció';
+
+  @override
+  String get flatrateProviders => 'Subscripció';
+
+  @override
+  String get rentProviders => 'Lloguer';
+
+  @override
+  String get buyProviders => 'Compra';
+
+  @override
+  String get allTypes => 'Tots';
+
+  @override
+  String get movies => 'Pel·lícules';
+
+  @override
+  String get tvshows => 'Sèries';
+
+  @override
+  String get sortAlphabetically => 'Alfabètic';
+
+  @override
+  String get sortRating => 'Puntuació';
+
+  @override
+  String get sortUserRating => 'Puntuació (Meva)';
+
+  @override
+  String get sortReleaseDate => 'Data d\'estrena';
+
+  @override
+  String get sortRuntime => 'Duració';
+
+  @override
+  String get genres => 'Gèneres';
+
+  @override
+  String get titles => 'Títols';
+
+  @override
+  String get rate => 'Valora';
+
+  @override
+  String get your_rate => 'La teva valoració';
+
+  @override
+  String get reset_rate => 'Elimina la valoració';
+
+  @override
+  String get emptyRates => 'Encara no has valorat cap títol.';
+
+  @override
+  String get watchlistTitle => 'Per veure';
+
+  @override
+  String get rateslistTitle => 'Valorades';
+
+  @override
+  String get yes => 'Sí';
+
+  @override
+  String get cancel => 'Cancel·la';
+
+  @override
+  String get originaTitle => 'Títol original';
+
+  @override
+  String get originalLanguage => 'Idioma original';
+
+  @override
+  String get originCountry => 'País';
+
+  @override
+  String get schemeSelectTitle => 'Sel·lecciona el color';
+
+  @override
+  String get defaultScheme => 'Negre';
+
+  @override
+  String get blueScheme => 'Blau';
+
+  @override
+  String get redScheme => 'Vermell';
+
+  @override
+  String get about => 'Quant a...';
+
+  @override
+  String get aboutDescription => 'Movie Scout el millor rastrejador de pel·lícules i sèries.';
+
+  @override
+  String get recommended => 'Recomanats';
+}
---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,214 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'Movie Scout';
+
+  @override
+  String get messageEmptyList => 'Aún no se ha seleccionado ninguna película.';
+
+  @override
+  String get messageEmptyList2 => 'Haz una búsqueda utilizando la lupa de la barra inferior.';
+
+  @override
+  String get messageEmptyList3 => 'Inicia sessión o registrate en TMDB desde el menú lateral.';
+
+  @override
+  String get search => 'Busca un título';
+
+  @override
+  String get searchTitle => 'Búsqueda';
+
+  @override
+  String get back => 'Atrás';
+
+  @override
+  String get username => 'Usuario';
+
+  @override
+  String get password => 'Contraseña';
+
+  @override
+  String get anonymousUser => 'Sin usuario';
+
+  @override
+  String get loginTitle => 'Inicio de sesión';
+
+  @override
+  String get loginDescription => 'Inicia sesión en tu cuenta de TMDB';
+
+  @override
+  String get login => 'Inicia sesión';
+
+  @override
+  String get logout => 'Cierra sesión';
+
+  @override
+  String get loginSuccess => 'Sesión iniciada correctamente.';
+
+  @override
+  String get loginFailed => 'Usuario o contraseña incorrectos.';
+
+  @override
+  String get logoutSuccess => 'Sesión cerrada correctamente.';
+
+  @override
+  String get loginToTmdb => 'Inicia sesión en TMDb';
+
+  @override
+  String get completeLoginToTmdb => 'Completa el inicio de sesión';
+
+  @override
+  String get signupToTmdb => 'Registrate en TMDb';
+
+  @override
+  String get signInToWatchlist => 'Es necesario iniciar sesión para añadir títulos.';
+
+  @override
+  String get tvShow => 'Serie';
+
+  @override
+  String get movie => 'Película';
+
+  @override
+  String get select => 'Selecciona';
+
+  @override
+  String get imdbImport => 'Importar de IMDB';
+
+  @override
+  String get imdbImportHint => 'Selecciona un archivo CSV exportado de IMDB';
+
+  @override
+  String get imdbImportWatchlist => 'Importar Para Ver';
+
+  @override
+  String get imdbImportRateslist => 'Importar Valoraciones';
+
+  @override
+  String get imdbImportCount => 'Títulos importados correctamente';
+
+  @override
+  String get imdbResetWatchlist => 'ELIMINAR PARA VER';
+
+  @override
+  String get imdbResetRateslist => 'ELIMINAR VALORACIONES';
+
+  @override
+  String get imdbConfirmationTitle => 'ATENCIÓN';
+
+  @override
+  String get imdbResetWatchlistConfirmation => '¿Seguro que quieres eliminar los títulos Para Ver?';
+
+  @override
+  String get imdbResetRateslistConfirmation => '¿Seguro que quieres eliminar las Valoraciones?';
+
+  @override
+  String get resetWatchlistCount => 'Títulos para ver: ';
+
+  @override
+  String get resetRateslistCount => 'Títulos valorados: ';
+
+  @override
+  String get missingDescription => 'Falta la descripción';
+
+  @override
+  String get flatrateProviders => 'Suscripción';
+
+  @override
+  String get rentProviders => 'Alquiler';
+
+  @override
+  String get buyProviders => 'Compra';
+
+  @override
+  String get allTypes => 'Todo';
+
+  @override
+  String get movies => 'Películas';
+
+  @override
+  String get tvshows => 'Series';
+
+  @override
+  String get sortAlphabetically => 'Alfabético';
+
+  @override
+  String get sortRating => 'Valoración';
+
+  @override
+  String get sortUserRating => 'Valoración (Mia)';
+
+  @override
+  String get sortReleaseDate => 'Fecha de estreno';
+
+  @override
+  String get sortRuntime => 'Duración';
+
+  @override
+  String get genres => 'Géneros';
+
+  @override
+  String get titles => 'Títulos';
+
+  @override
+  String get rate => 'Valora';
+
+  @override
+  String get your_rate => 'Tu valoración';
+
+  @override
+  String get reset_rate => 'Elimina tu valoración';
+
+  @override
+  String get emptyRates => 'Todavía no has valorado ningún título.';
+
+  @override
+  String get watchlistTitle => 'Por ver';
+
+  @override
+  String get rateslistTitle => 'Valoradas';
+
+  @override
+  String get yes => 'Sí';
+
+  @override
+  String get cancel => 'Cancela';
+
+  @override
+  String get originaTitle => 'Título original';
+
+  @override
+  String get originalLanguage => 'Idioma original';
+
+  @override
+  String get originCountry => 'País';
+
+  @override
+  String get schemeSelectTitle => 'Selecciona el color';
+
+  @override
+  String get defaultScheme => 'Negro';
+
+  @override
+  String get blueScheme => 'Azul';
+
+  @override
+  String get redScheme => 'Rojo';
+
+  @override
+  String get about => 'Sobre...';
+
+  @override
+  String get aboutDescription => 'Movie Scout el mejor rastreador de películas y series.';
+
+  @override
+  String get recommended => 'Recomendados';
+}
