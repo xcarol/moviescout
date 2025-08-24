38a7899a964db93dcffceaaa168fbb144d507ae1
xcarol <xcarolr@gmail.com>
2025-08-24 17:31:53 +0200

    refactor: replace string constants with static const in TmdbProvider in order to make them public

--- lib/models/tmdb_provider.dart
+++ lib/models/tmdb_provider.dart
@@ -1,27 +1,32 @@
-// ignore_for_file: constant_identifier_names, unused_element
-
-const _logo_path = 'logo_path';
-const _provider_id = 'provider_id';
-const _provider_name = 'provider_name';
-const _display_priority = 'display_priority';
-
 class TmdbProvider {
+  static const logoPathName = 'logo_path';
+  static const providerId = 'provider_id';
+  static const providerName = 'provider_name';
+  static const providerEnabled = 'enabled';
+
   final Map _provider;
 
   const TmdbProvider({required Map<dynamic, dynamic> provider})
       : _provider = provider;
 
   String get name {
-    if (_provider[_provider_name] != null) {
-      return _provider[_provider_name].toString();
+    if (_provider[providerName] != null) {
+      return _provider[providerName].toString();
     }
     return '';
   }
-  
+
+  int get id {
+    if (_provider[providerId] != null) {
+      return _provider[providerId] as int;
+    }
+    return 0;
+  }
+
   String get logoPath {
-    if (_provider[_logo_path] != null) {
-      return (_provider[_logo_path] as String).isNotEmpty
-          ? 'https://image.tmdb.org/t/p/w92${_provider[_logo_path]}'
+    if (_provider[logoPathName] != null) {
+      return (_provider[logoPathName] as String).isNotEmpty
+          ? 'https://image.tmdb.org/t/p/w92${_provider[logoPathName]}'
           : '';
     }
     return '';
