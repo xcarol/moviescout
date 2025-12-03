ddf1d42d6566937a17ed4a90bb94f5c53ec9ccff
xcarol <xcarolr@gmail.com>
2025-12-03 18:45:29 +0100

    feat: improve recommendations by providing the recommended titles of the titles the user has rated

--- lib/services/discoverlist_service.dart
+++ lib/services/discoverlist_service.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:isar/isar.dart';
+import 'package:moviescout/models/tmdb_title.dart';
+import 'package:moviescout/services/isar_service.dart';
 import 'package:moviescout/services/tmdb_base_service.dart';
 import 'package:moviescout/services/tmdb_list_service.dart';
 
@@ -10,35 +13,79 @@ class TmdbDiscoverlistService extends TmdbListService {
   TmdbDiscoverlistService(super.listName);
 
   Future<void> retrieveDiscoverlist(
-    String accountId,
-    String sessionId,
-    Locale locale) async {
-    retrieveList(accountId.isEmpty ? anonymousAccountId : accountId, retrieveMovies: () async {
-      return getTitlesFromServer((int page) async {
-        if (page > 2) {
-          return (statusCode: 200, body: '{}');
-        }
-
-        return get(_tmdbPopularlistMovies
-            .replaceFirst('{ACCOUNT_ID}', accountId)
-            .replaceFirst('{SESSION_ID}', sessionId)
-            .replaceFirst('{PAGE}', page.toString())
-            .replaceFirst(
-                '{LOCALE}', '${locale.languageCode}-${locale.countryCode}'));
-      });
+      String accountId, String sessionId, Locale locale) async {
+    retrieveList(accountId.isEmpty ? anonymousAccountId : accountId,
+        retrieveMovies: () async {
+      return _getDiscoveryTitles(
+          accountId, sessionId, locale, 'movie', _tmdbPopularlistMovies);
     }, retrieveTvshows: () async {
-      return getTitlesFromServer((int page) async {
+      return _getDiscoveryTitles(
+          accountId, sessionId, locale, 'tv', _tmdbPopularlistTv);
+    });
+  }
+
+  Future<List> _getDiscoveryTitles(String accountId, String sessionId,
+      Locale locale, String mediaType, String popularEndpoint) async {
+    final isar = IsarService.instance;
+    final ratedTitles = await isar.tmdbTitles
+        .filter()
+        .ratingGreaterThan(0)
+        .and()
+        .mediaTypeEqualTo(mediaType)
+        .sortByRatingDesc()
+        .findAll();
+
+    final watchlistTitles = await isar.tmdbTitles
+        .filter()
+        .listNameEqualTo('watchlist')
+        .and()
+        .mediaTypeEqualTo(mediaType)
+        .findAll();
+
+    final Set<int> ratedIds = ratedTitles.map((t) => t.tmdbId).toSet();
+    final Set<int> watchlistIds = watchlistTitles.map((t) => t.tmdbId).toSet();
+    final List<dynamic> recommendations = [];
+    final Set<int> addedIds = {};
+
+    // Exclude titles already in watchlist
+    addedIds.addAll(watchlistIds);
+
+    for (final title in ratedTitles) {
+      if (recommendations.length >= 40) break;
+      for (final rec in title.recommendations) {
+        if (recommendations.length >= 40) break;
+        final recId = rec['id'];
+        if (!ratedIds.contains(recId) && !addedIds.contains(recId)) {
+          recommendations.add(rec);
+          addedIds.add(recId);
+        }
+      }
+    }
+
+    if (recommendations.length < 40) {
+      final popularTitles = await getTitlesFromServer((int page) async {
         if (page > 2) {
           return (statusCode: 200, body: '{}');
         }
 
-        return get(_tmdbPopularlistTv
+        return get(popularEndpoint
             .replaceFirst('{ACCOUNT_ID}', accountId)
             .replaceFirst('{SESSION_ID}', sessionId)
             .replaceFirst('{PAGE}', page.toString())
             .replaceFirst(
                 '{LOCALE}', '${locale.languageCode}-${locale.countryCode}'));
       });
-    });
+
+      for (final title in popularTitles) {
+        if (recommendations.length >= 40) break;
+        final id = title['id'];
+        if (!ratedIds.contains(id) && !addedIds.contains(id)) {
+          recommendations.add(title);
+          addedIds.add(id);
+        }
+      }
+    }
+
+    return recommendations;
   }
 }
