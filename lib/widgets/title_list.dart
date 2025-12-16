81fdfcac6e0df9cbf05b8640f495c0fa5880b2d2
xcarol <xcarolr@gmail.com>
2025-12-16 19:00:01 +0100

    feat: refactor TitleList to extract the logic to a controller

--- lib/widgets/title_list.dart
+++ lib/widgets/title_list.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:collection/collection.dart' show ListEquality;
 import 'package:moviescout/l10n/app_localizations.dart';
 import 'package:moviescout/models/tmdb_provider.dart';
-import 'package:moviescout/models/tmdb_title.dart';
-import 'package:moviescout/services/preferences_service.dart';
 import 'package:moviescout/services/tmdb_list_service.dart';
 import 'package:moviescout/services/tmdb_provider_service.dart';
 import 'package:moviescout/widgets/drop_down_selector.dart';
 import 'package:moviescout/widgets/title_card.dart';
 import 'package:moviescout/widgets/title_list_control_panel.dart';
 import 'package:provider/provider.dart';
+import 'package:moviescout/widgets/title_list_controller.dart';
 
 class TitleList extends StatefulWidget {
   final TmdbListService listService;
@@ -22,154 +21,35 @@ class TitleList extends StatefulWidget {
 }
 
 class _TitleListState extends State<TitleList> {
-  final FocusNode _searchFocusNode = FocusNode();
-  final ScrollController _scrollController = ScrollController();
-  bool _isSortAsc = true;
-  late bool _showFilters;
-  late String _showFiltersPreferencesName;
-  late String _textFilterPreferencesName;
-  late String _selectedGenresPreferencesName;
-  late String _selectedTypePreferencesName;
-  late String _selectedSortPreferencesName;
-  late String _filterByProvidersPreferencesName;
-  late String _sortPreferencesName;
-  String _selectedType = '';
-  late List<String> _titleTypes;
-  String _selectedSort = '';
-  late List<String> _titleSorts;
-  List<String> _selectedGenres = [];
-  late bool _filterByProviders = false;
-  List<int> _providerListIds = [];
-  late TextEditingController _textFilterController;
+  late final TitleListController _controller;
 
   @override
   void initState() {
     super.initState();
-    _textFilterController = TextEditingController();
-
-    _textFilterPreferencesName = '${widget.listService.listName}_TextFilter';
-    _textFilterController.text =
-        PreferencesService().prefs.getString(_textFilterPreferencesName) ?? '';
-
-    _showFiltersPreferencesName = '${widget.listService.listName}_ShowFilters';
-    _showFilters =
-        PreferencesService().prefs.getBool(_showFiltersPreferencesName) ??
-            false;
-
-    _selectedTypePreferencesName =
-        '${widget.listService.listName}_SelectedType';
-    _selectedType =
-        PreferencesService().prefs.getString(_selectedTypePreferencesName) ??
-            '';
-
-    _selectedGenresPreferencesName =
-        '${widget.listService.listName}_SelectedGenres';
-    _selectedGenres = PreferencesService()
-            .prefs
-            .getStringList(_selectedGenresPreferencesName) ??
-        [];
-
-    _filterByProvidersPreferencesName =
-        '${widget.listService.listName}_FilterByProviders';
-    _filterByProviders =
-        PreferencesService().prefs.getBool(_filterByProvidersPreferencesName) ??
-            false;
-
-    _sortPreferencesName = '${widget.listService.listName}_Sort';
-    _isSortAsc =
-        PreferencesService().prefs.getBool(_sortPreferencesName) ?? true;
-
-    _selectedSortPreferencesName =
-        '${widget.listService.listName}_SelectedSort';
-    _selectedSort =
-        PreferencesService().prefs.getString(_selectedSortPreferencesName) ??
-            '';
-
-    widget.listService.addListener(_onListServiceChanged);
+    _controller = TitleListController(widget.listService);
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      _setupFilters();
+      _controller.initializeControlLocalizations(context);
+      await _controller.setupFilters();
     });
   }
 
   @override
   void dispose() {
-    widget.listService.removeListener(_onListServiceChanged);
-    _textFilterController.dispose();
-    _searchFocusNode.dispose();
-    _scrollController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
-  didChangeDependencies() {
+  void didChangeDependencies() {
     super.didChangeDependencies();
     final isCurrent = ModalRoute.of(context)?.isCurrent ?? false;
-    if (!isCurrent && _searchFocusNode.hasFocus) {
-      _searchFocusNode.unfocus();
+    if (!isCurrent && _controller.searchFocusNode.hasFocus) {
+      _controller.searchFocusNode.unfocus();
     }
-    _initilizeControlLocalizations();
-  }
-
-  void _onListServiceChanged() {
     if (mounted) {
-      _initilizeControlLocalizations();
+      _controller.initializeControlLocalizations(context);
     }
-  }
-
-  void _initilizeControlLocalizations() {
-    setState(() {
-      final localizations = AppLocalizations.of(context)!;
-      if (_selectedType.isEmpty) {
-        _selectedType = localizations.allTypes;
-      }
-      _titleTypes = [
-        localizations.allTypes,
-        localizations.movies,
-        localizations.tvshows,
-      ];
-      if (_selectedSort.isEmpty) {
-        _selectedSort = localizations.sortAlphabetically;
-      }
-      _titleSorts = [
-        localizations.sortAlphabetically,
-        localizations.sortRating,
-        if (widget.listService.userRatingAvailable)
-          localizations.sortUserRating,
-        if (widget.listService.userRatedDateAvailable)
-          localizations.sortDateRated,
-        localizations.sortReleaseDate,
-        localizations.sortRuntime,
-        localizations.sortAddedOrder,
-      ];
-    });
-  }
-
-  Future<void> _setupFilters() async {
-    try {
-      await widget.listService.setFilters(
-        text: _textFilterController.text,
-        type: _titleTypeToOption(_selectedType),
-        genres: _selectedGenres,
-        filterByProviders: _filterByProviders,
-        providerListIds: _providerListIds,
-      );
-    } catch (_) {}
-  }
-
-  List<int> _retrieveUserProviders(TmdbProviderService providerService) {
-    if (providerService.isInitialized == false) {
-      return [];
-    }
-
-    final map = providerService.providers;
-
-    final enabledProviders = map.entries
-        .where((entry) => entry.value[TmdbProvider.providerEnabled] == 'true')
-        .map((entry) => int.parse(entry.value[TmdbProvider.providerId]!))
-        .toList();
-
-    return enabledProviders;
   }
 
   Widget _titleList() {
@@ -179,28 +59,16 @@ class _TitleListState extends State<TitleList> {
         builder: (context, service, _) {
           return NotificationListener<ScrollNotification>(
             onNotification: (scrollInfo) {
-              final metrics = scrollInfo.metrics;
-              final currentScroll = metrics.pixels;
-
-              final itemHeight = TitleCard.cardHeight;
-              final firstVisibleIndex = (currentScroll / itemHeight).floor();
-              final visibleItemCount =
-                  (metrics.viewportDimension / itemHeight).ceil();
-              final lastVisibleIndex = firstVisibleIndex + visibleItemCount;
-
-              if (!service.isLoading.value &&
-                  service.hasMore &&
-                  lastVisibleIndex >= service.loadedTitleCount - 3) {
-                service.loadNextPage();
-              }
+              _controller.onScrollNotification(
+                  scrollInfo, TitleCard.cardHeight);
               return false;
             },
             child: Flexible(
               child: Scrollbar(
-                controller: _scrollController,
+                controller: _controller.scrollController,
                 child: ListView.builder(
                   key: const PageStorageKey('TitleListView'),
-                  controller: _scrollController,
+                  controller: _controller.scrollController,
                   shrinkWrap: true,
                   cacheExtent: 2000.0,
                   itemCount: service.loadedTitleCount,
@@ -252,35 +120,19 @@ class _TitleListState extends State<TitleList> {
             children: [
               TitleListControlPanel(
                 textFilterChanged: (newTextFilter) {
-                  setState(() {
-                    widget.listService.setTextFilter(newTextFilter);
-                  });
-                  PreferencesService()
-                      .prefs
-                      .setString(_textFilterPreferencesName, newTextFilter);
+                  _controller.setTextFilter(newTextFilter);
                 },
-                textFilterController: _textFilterController,
-                selectedGenres: _selectedGenres.toList(),
+                textFilterController: _controller.textFilterController,
+                selectedGenres: _controller.selectedGenres.toList(),
                 genresList: genres,
                 genresChanged: (List<String> genresChanged) {
-                  setState(() {
-                    _selectedGenres = genresChanged.toList();
-                    widget.listService.setGenresFilter(_selectedGenres);
-                  });
-                  PreferencesService().prefs.setStringList(
-                      _selectedGenresPreferencesName, _selectedGenres);
+                  _controller.setGenres(genresChanged);
                 },
-                filterByProviders: _filterByProviders,
+                filterByProviders: _controller.filterByProviders,
                 providersChanged: (bool providersChanged) {
-                  setState(() {
-                    _filterByProviders = providersChanged;
-                    widget.listService.setProvidersFilter(
-                        _filterByProviders, _providerListIds);
-                  });
-                  PreferencesService().prefs.setBool(
-                      _filterByProvidersPreferencesName, _filterByProviders);
+                  _controller.setFilterByProviders(providersChanged);
                 },
-                focusNode: _searchFocusNode,
+                focusNode: _controller.searchFocusNode,
               ),
               Container(
                 color: Theme.of(context).colorScheme.primaryContainer,
@@ -296,9 +148,9 @@ class _TitleListState extends State<TitleList> {
   }
 
   Widget _infoLine() {
-    final anyFilterActive = _selectedGenres.isNotEmpty ||
-        _filterByProviders ||
-        _textFilterController.text.isNotEmpty;
+    final anyFilterActive = _controller.selectedGenres.isNotEmpty ||
+        _controller.filterByProviders ||
+        _controller.textFilterController.text.isNotEmpty;
 
     return Container(
       color: Theme.of(context).colorScheme.onPrimaryContainer,
@@ -340,15 +192,12 @@ class _TitleListState extends State<TitleList> {
               visualDensity: VisualDensity.compact,
             ),
             onPressed: () {
-              setState(() {
-                _showFilters = !_showFilters;
-                PreferencesService()
-                    .prefs
-                    .setBool(_showFiltersPreferencesName, _showFilters);
-              });
+              _controller.toggleFilters();
             },
             icon: Icon(
-              _showFilters ? Icons.filter_list_off : Icons.filter_list,
+              _controller.showFilters
+                  ? Icons.filter_list_off
+                  : Icons.filter_list,
               color: anyFilterActive
                   ? Theme.of(context).colorScheme.onPrimary
                   : Theme.of(context).colorScheme.primary,
@@ -359,24 +208,13 @@ class _TitleListState extends State<TitleList> {
     );
   }
 
-  String _titleTypeToOption(String type) {
-    String selectedType = '';
-
-    if (type == AppLocalizations.of(context)!.movies) {
-      selectedType = 'movie';
-    } else if (type == AppLocalizations.of(context)!.tvshows) {
-      selectedType = 'tv';
-    }
-
-    return selectedType;
-  }
-
   List<Widget> _buildTypeAndCountWidgets() {
-    final selectedType = _titleTypeToOption(_selectedType);
-    final textColor = selectedType == ''
+    final typeOption = _titleTypeToOption(_controller.selectedType);
+
+    final textColor = typeOption == ''
         ? Theme.of(context).colorScheme.primary
         : Theme.of(context).colorScheme.onPrimary;
-    final backgroundColor = selectedType == ''
+    final backgroundColor = typeOption == ''
         ? Theme.of(context).colorScheme.onPrimary
         : Theme.of(context).colorScheme.primary;
 
@@ -402,15 +240,9 @@ class _TitleListState extends State<TitleList> {
             );
           },
         ),
-        selectedOption: _selectedType,
-        options: _titleTypes,
-        onSelected: (value) => setState(() {
-          _selectedType = value;
-          widget.listService.setTypeFilter(_titleTypeToOption(value));
-          PreferencesService()
-              .prefs
-              .setString(_selectedTypePreferencesName, _selectedType);
-        }),
+        selectedOption: _controller.selectedType,
+        options: _controller.titleTypes,
+        onSelected: (value) => _controller.setSelectedType(context, value),
         arrowIcon: Icon(
           Icons.arrow_drop_down,
           color: textColor,
@@ -419,39 +251,19 @@ class _TitleListState extends State<TitleList> {
     ];
   }
 
-  String _sortNameToOption(BuildContext context, String name) {
-    if (name == AppLocalizations.of(context)!.sortAlphabetically) {
-      return SortOption.alphabetically;
-    } else if (name == AppLocalizations.of(context)!.sortRating) {
-      return SortOption.rating;
-    } else if (name == AppLocalizations.of(context)!.sortUserRating) {
-      return SortOption.userRating;
-    } else if (name == AppLocalizations.of(context)!.sortDateRated) {
-      return SortOption.dateRated;
-    } else if (name == AppLocalizations.of(context)!.sortReleaseDate) {
-      return SortOption.releaseDate;
-    } else if (name == AppLocalizations.of(context)!.sortRuntime) {
-      return SortOption.runtime;
-    } else if (name == AppLocalizations.of(context)!.sortAddedOrder) {
-      return SortOption.addedOrder;
-    } else {
-      return SortOption.alphabetically;
-    }
+  String _titleTypeToOption(String type) {
+    final localizations = AppLocalizations.of(context);
+    if (type == localizations?.movies) return 'movie';
+    if (type == localizations?.tvshows) return 'tv';
+    return '';
   }
 
   Widget _sortSelector() {
     return DropdownSelector(
-      selectedOption: _selectedSort,
-      options: _titleSorts,
-      onSelected: (value) => setState(() {
-        _selectedSort = value;
-        PreferencesService()
-            .prefs
-            .setString(_selectedSortPreferencesName, _selectedSort);
-        widget.listService
-            .setSort(_sortNameToOption(context, value), _isSortAsc);
-      }),
-      arrowIcon: _isSortAsc
+      selectedOption: _controller.selectedSort,
+      options: _controller.titleSorts,
+      onSelected: (value) => _controller.setSelectedSort(context, value),
+      arrowIcon: _controller.isSortAsc
           ? Icon(
               Icons.arrow_drop_down,
               color: Theme.of(context).colorScheme.primary,
@@ -468,54 +280,68 @@ class _TitleListState extends State<TitleList> {
       color: Theme.of(context).colorScheme.primary,
       icon: Icon(Icons.swap_vert),
       onPressed: () {
-        setState(() {
-          _isSortAsc = !_isSortAsc;
-          PreferencesService().prefs.setBool(_sortPreferencesName, _isSortAsc);
-          widget.listService
-              .setSort(_sortNameToOption(context, _selectedSort), _isSortAsc);
-        });
+        _controller.toggleSortDirection(context);
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<TmdbProviderService>(
-      builder: (context, providerService, _) {
-        final providerList = _retrieveUserProviders(providerService);
+    return ListenableBuilder(
+        listenable: _controller,
+        builder: (context, child) {
+          return Consumer<TmdbProviderService>(
+            builder: (context, providerService, _) {
+              final providerList = _retrieveUserProviders(providerService);
 
-        if (!const ListEquality().equals(providerList, _providerListIds)) {
-          _providerListIds = providerList;
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            widget.listService
-                .setProvidersFilter(_filterByProviders, _providerListIds);
-          });
-        }
+              if (!const ListEquality()
+                  .equals(providerList, _controller.providerListIds)) {
+                // Schedule update
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  _controller.setProviderListIds(providerList);
+                });
+              }
 
-        return Expanded(
-          child: Container(
-            color: Theme.of(context).colorScheme.onPrimaryContainer,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
+              return Expanded(
+                child: Container(
                   color: Theme.of(context).colorScheme.onPrimaryContainer,
-                  child: Divider(
-                    color: Theme.of(context).colorScheme.primaryContainer,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        child: Divider(
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                        ),
+                      ),
+                      _infoLine(),
+                      Divider(
+                        height: 1,
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                      ),
+                      if (_controller.showFilters) _controlPanel(),
+                      _titleList(),
+                    ],
                   ),
                 ),
-                _infoLine(),
-                Divider(
-                  height: 1,
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                ),
-                if (_showFilters) _controlPanel(),
-                _titleList(),
-              ],
-            ),
-          ),
-        );
-      },
-    );
+              );
+            },
+          );
+        });
+  }
+
+  List<int> _retrieveUserProviders(TmdbProviderService providerService) {
+    if (providerService.isInitialized == false) {
+      return [];
+    }
+
+    final map = providerService.providers;
+
+    final enabledProviders = map.entries
+        .where((entry) => entry.value[TmdbProvider.providerEnabled] == 'true')
+        .map((entry) => int.parse(entry.value[TmdbProvider.providerId]!))
+        .toList();
+
+    return enabledProviders;
   }
 }
