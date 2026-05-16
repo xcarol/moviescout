953b4d149da5821df931d1e742c1e977882a2af3
xcarol <xcarolr@gmail.com>
2026-05-16 19:23:46 +0200

    chore: remove unused import

--- lib/widgets/snooze_filter_tabs.dart
+++ lib/widgets/snooze_filter_tabs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/symbols.dart';
 import 'package:moviescout/l10n/app_localizations.dart';
 import 'package:moviescout/models/custom_colors.dart';
 import 'package:moviescout/services/tmdb_list_service.dart';
