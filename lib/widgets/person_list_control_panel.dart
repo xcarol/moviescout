3ffece8840b08d807762a63075c686c6e399ea1f
xcarol <xcarolr@gmail.com>
2026-06-20 17:16:44 +0200

    feat: create new TextFilterWidtge and use it in title and person control panels.

--- lib/widgets/person_list_control_panel.dart
+++ lib/widgets/person_list_control_panel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moviescout/l10n/app_localizations.dart';
-import 'package:moviescout/models/title_list_theme.dart';
+import 'package:moviescout/widgets/text_filter_widget.dart';
 
 class PersonListControlPanel extends StatelessWidget {
   final TextEditingController textFilterController;
@@ -16,65 +16,17 @@ class PersonListControlPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final titleTheme = Theme.of(context).extension<TitleListTheme>()!;
     final localizations = AppLocalizations.of(context)!;
 
     return Container(
       padding: const EdgeInsets.all(4.0),
       child: Column(
         children: [
-          SizedBox(
-            height: 26,
-            child: Theme(
-              data: Theme.of(context).copyWith(
-                textSelectionTheme: TextSelectionThemeData(
-                  selectionColor: titleTheme.searchSelectionColor,
-                  selectionHandleColor: titleTheme.searchCursorColor,
-                ),
-              ),
-              child: TextField(
-                controller: textFilterController,
-                focusNode: focusNode,
-                style: TextStyle(
-                    color: titleTheme.controlPanelForeground,
-                    fontSize: 14,
-                    fontWeight: textFilterController.text.isEmpty
-                        ? FontWeight.normal
-                        : FontWeight.bold),
-                cursorColor: titleTheme.searchCursorColor,
-                cursorHeight: 16,
-                decoration: InputDecoration(
-                  isDense: true,
-                  hintText: localizations.searchPerson,
-                  hintStyle: TextStyle(color: titleTheme.searchHintColor),
-                  suffixIconColor: titleTheme.controlPanelForeground,
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 5),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide:
-                        BorderSide(color: titleTheme.controlPanelForeground),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide:
-                        BorderSide(color: titleTheme.controlPanelForeground),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide:
-                        BorderSide(color: titleTheme.controlPanelForeground),
-                  ),
-                  suffixIcon: GestureDetector(
-                    child: Icon(Icons.clear),
-                    onTap: () {
-                      textFilterController.clear();
-                      onTextChanged('');
-                    },
-                  ),
-                ),
-                onChanged: onTextChanged,
-              ),
-            ),
+          TextFilterWidget(
+            controller: textFilterController,
+            focusNode: focusNode,
+            hintText: localizations.searchPerson,
+            onChanged: onTextChanged,
           ),
         ],
       ),
