135fc9eb673cff030e98242f515ffbc07a3016ed
xcarol <xcarolr@gmail.com>
2026-07-17 19:39:06 +0200

    fix: to center the three dots

--- lib/widgets/expandable_text.dart
+++ lib/widgets/expandable_text.dart
@@ -69,11 +69,13 @@ class _ExpandableTextState extends State<ExpandableText> {
                     _isExpanded = !_isExpanded;
                   });
                 },
-                child: Icon(
-                  Icons.more_horiz,
-                  color: _isExpanded
-                      ? Colors.transparent
-                      : Theme.of(context).colorScheme.onSurfaceVariant,
+                child: Center(
+                  child: Icon(
+                    Icons.more_horiz,
+                    color: _isExpanded
+                        ? Colors.transparent
+                        : Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
                 ),
               ),
           ],
